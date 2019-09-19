5e219d922cfc3a938c958a396322850342d0a107
Takshil Khorasia <takshil@techzillaindia.com>
2019-09-20 00:03:38 +0530

    Added scripts to scrape products, Title, heading, Will put data to db soon

--- IndiaMartScraping/IndiaMartScraping/spiders/mainPageScraper.py
+++ IndiaMartScraping/IndiaMartScraping/spiders/mainPageScraper.py
@@ -9,9 +9,24 @@ class IndiaMartScraper(scrapy.Spider):
         yield scrapy.Request(url=url, callback=self.detailsParser)
 
     def detailsParser(self, response):
-        group = response.css('div.product-sec-wrapper>h2::text').get()
-        print(group)
-        # img = response.css('div.product-banner.pbnr1::attr(style)').get()
-        # print(img,">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        img = response.css('div.product-item>div.product-thumbnail>a>img::attr(data-original)').get()
-        print(img)
+        pageTitle = response.css('title::text').get()
+        parentProductDiv = response.css('div.product-sec-wrapper')
+        for childDiv in parentProductDiv:
+            group = childDiv.css('h2::text').get()
+            categoryName = childDiv.css('div.product-item>div.product-meta>h3>a::text').getall()
+            categoryImg = childDiv.css('div.product-item>div.product-thumbnail>a>img::attr(data-original)').getall()
+            print(group,"Group>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            print(categoryName,"BoldCategory>>>>>>>>>>>>>>>>>")
+            print(categoryImg,"Images>>>>>>>>>>>>>>>>>>>>>")
+            categoryParent = childDiv.css('div.product-item')
+            for subCategoryDiv in categoryParent:
+                subCategoryName = subCategoryDiv.css('div.product-meta>p>a::text').getall()
+                print(subCategoryName,"subcategory >>>>>>>>>>>>>>>>>>")
+        print(pageTitle)
+        pageMainHeading = "".join(response.css('div#connect-buyer>div>h2 *::text').getall())
+        pageSubHeading = response.css('div#connect-buyer>div>p::text').get()
+        pageMainTags = response.css('section#connect-buyer-wrapper div#connect-buyer>div>ul')
+        print(pageMainHeading,pageSubHeading)
+        for childLi in pageMainTags:
+            tags = response.css('li>p::text').getall()
+            print(tags[0]+" "+tags[1]+" "+tags[2])
